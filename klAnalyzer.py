0e1b16bd5490a9817f913c295f2e305017e33585
Skyikeli <fencingworld@qq.com>
2016-12-21 12:52:18 +0800

    Update klAnalyzer.py

--- klAnalyzer.py
+++ klAnalyzer.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Dec 20 11:52:24 2016
 
-@author: keli
+@author: hihi
 """
 import os
 import codecs
@@ -10,12 +10,7 @@ import chardet
 import re
 #import sys
 #print sys.getdefaultencoding()
-def is_chinese(uchar):
-        """判断一个unicode是否是汉字"""
-        if u'\u4e00' <= uchar <= u'\u9fff':
-                return True
-        else:
-                return False
+
 
 path = r'G:\workspace\nlp\materials'
 raw_materials = r'raw_materials'
@@ -23,91 +18,37 @@ utf_materials = r'utf_materials'
 uni_materials = r'uni_materials'
 pre_materials = r'pre_materials'
 txt = r'960101.TXT'
-charSetDel = [u'　',u'＊',u'■',u'\n',u'\r',u'\t',u'|',u' ',
-           #   u'0',u'',u'2',u'3',u'4',u'5',u'6',u'7',u'8',u'9',u'[',u']'
-              ]
+charSetDel = [u'　',u'＊',u'■',u'\n',u'\r',u'\t',u'|',u' ' ]
 for i in range(128):
     #print i,chr(i),type(chr(i))
     charSetDel.append(chr(i).decode('utf-8'))
 
-print os.path.getsize(os.path.join(path,uni_materials,txt))
+#print os.path.getsize(os.path.join(path,uni_materials,txt))
 fp = codecs.open(os.path.join(path,uni_materials,txt),"r",'utf-16')
 fp_r = codecs.open(os.path.join(path,pre_materials,txt),"w+",'utf-16')
-
-
 content = fp.read()
-#content = content.decode('utf-8')
+print len(content)
+#remove unrelated char and English char
 for c in charSetDel:
-    #print len(content)
     content = content.replace(c,'')
+print len(content)
+#mark chinese numbers
+#for example 一九九五年十二,一百二十万,一些,十四大,N亿吨,七五,无一退货,短斤少两
+num_ch = '[０-９．％]+'.decode('utf-8')
+content = re.sub(num_ch,'N',content)
+print len(content)
+spt_ch = '[。，、；：（）《》“”！／]+'.decode('utf-8')
+content = re.sub(spt_ch,'\n',content)
+print len(content)
 
-##content = content.decode('utf-8')
-#print len(content)
-#for c in content:
-#    print ord(c)
-#    h =chardet.detect(c)
-#    if  h['encoding']!='windows-1252'and  h['encoding']!=None:
-#        print c.encode(h['encoding']),h['encoding']
-        
-#print chardet.detect(content)
-print len(content)#,type(content)
-#c = []
-#c.append( content[10])
-#for i in range(len(content)):
-#    if not ('\u4e00'<=content[i] and content[i]<='\u9fa5'):
-#        print content[i],type(content[i])
-#print content[9],content[10],content[11]
+news_ch ='N第N版'.decode('utf-8')
+content = re.sub(news_ch,'\n',content)
+print len(content)
+
+news_ch ='国际N短讯'.decode('utf-8')
+content = re.sub(news_ch,'\n',content)
+print len(content)
+
 fp_r.write(content)
-#h=fp.read()
-#h  = h.decode('utf-8', 'ignore')
-#p = re.compile(ur'[^\u4e00-\u9fa5]') 
-#x = p.split(h)
-
-#fp_r.write(x)
-#while (h):
-    #print h
-#    line_coding =  chardet.detect(h)
-#    if line_coding['encoding']!='windows-1252' and line_coding['encoding']!='ascii':
-#        print line_coding,line_coding['encoding']
-#        print h
-#    h = fp.read(1)
-#for line in fp:
-##    #line =line[0:-1]
-##    for c in charSetDel:
-##        line = line.replace(c,'')
-#    line_coding =  chardet.detect(line)
-#    
-#    if line_coding['encoding'] !='utf-8':
-#        print line_coding['encoding']
-##        line  = line.decode(line_coding['encoding'], 'ignore')
-##        line  = line.encode('utf-8')
-#        print line
-#    else:
-#        print line,len(line)
-##    for ch in line:
-##        print is_chinese(line[0:1])
-#    
-#
-#    
-#    fp_r.write(line)
-#    x = chardet.detect(line)
-#    if x['encoding']!='utf-8':
-#        print line,len(line)
-#    #print chardet.detect(line)
-#    #fp_r.write(line.lstrip())
-#    #break 
-#    #print line
 fp.close()
 fp_r.close()
-#print os.path.getsize(os.path.join(path,pre_materials,txt))
-#
-#fp = codecs.open(os.path.join(path,pre_materials,txt),"r")
-#lines = fp.readlines()
-#i = 0
-#for line in lines:
-#    i= i+1
-#    line_coding =  chardet.detect(line) 
-#    if line_coding['encoding']!='utf-8':
-#        print line,line_coding['encoding'],i
-#    
-#fp.close()
